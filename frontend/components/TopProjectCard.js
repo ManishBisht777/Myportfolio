2ca4357c969f19b8fbf61357decdd2d7ec341679
manish <Manishbisht9711@gmail.com>
2022-03-19 16:31:45 +0530

    style changes in top project

--- frontend/components/TopProjectCard.js
+++ frontend/components/TopProjectCard.js
@@ -1,16 +1,18 @@
-import Image from "next/image";
 import styles from "../styles/TopProjectCard.module.css";
+import Image from "next/image";
 
 const TopProjectCard = ({ image, name, id }) => {
   return (
     <div className={styles.imagecontainer}>
-      <Image
-        src={image}
-        alt={name}
-        height={220}
-        width={220}
-        objectFit="cover"
-      />
+      <div className={styles.imagediv}>
+        <Image
+          src={image}
+          alt={name}
+          height={250}
+          width={220}
+          objectFit="cover"
+        />
+      </div>
       <h2 className={styles.projectnumber}>{id}</h2>
     </div>
   );
