f634cc15616a777a919f62f1a01f843f1df52e97
manish <Manishbisht9711@gmail.com>
2022-03-20 16:18:13 +0530

    landing page for now

--- frontend/components/Landingpage.js
+++ frontend/components/Landingpage.js
@@ -1,11 +1,25 @@
 import React from "react";
 import style from "../styles/landingpage.module.css";
+
 const Landingpage = () => {
   return (
     <div className={style.homepage}>
       <div className={style.introduction}>Hi There</div>
-      <div className={style.name}>
-        I am Man <span>i</span> sh
+      <div>
+        <h4 className={style.name}>
+          I am Man <span>i</span> sh
+        </h4>
+      </div>
+      <h4 className={style.role}>FrontEnd Developer</h4>
+      <p className={style.description}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus ea,
+        esse minus recusandae repellendus provident est in! Repellat numquam
+        consequuntur dolores unde repudiandae tempore quas corporis, quod
+        quidem, iusto fugit?
+      </p>
+      <div className={style.buttonpair}>
+        <button className={style.explore}>Explore</button>
+        <button className={style.explore}>Resume</button>
       </div>
     </div>
   );
