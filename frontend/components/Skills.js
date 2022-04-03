74f7e47306e8ad715420abc2ec7db48460dc8dbc
manish <Manishbisht9711@gmail.com>
2022-04-03 22:14:08 +0530

    skills container

--- frontend/components/Skills.js
+++ frontend/components/Skills.js
@@ -1,18 +1,27 @@
 import React from "react";
 import style from "../styles/skills.module.css";
+import { motion } from "framer-motion";
 
 const Skills = () => {
   return (
-    <div className={style.skillbox}>
-      <div className={style.container}>
+    <motion.div className={style.skillbox}>
+      {/* <div className={style.container}>
         <div className={style.left}></div>
         <div className={style.front}></div>
         <div className={style.right}></div>
         <div className={style.back}></div>
-
-        {/* <div className={style.shadow}></div> */}
-      </div>
-    </div>
+      </div> */}
+      <motion.div className={style.skills}>
+        <motion.div className={style.skill}>Html</motion.div>
+        <motion.div className={style.skill}>Css</motion.div>
+        <motion.div className={style.skill}>JavaScript</motion.div>
+        <motion.div className={style.skill}>NodeJS</motion.div>
+        <motion.div className={style.skill}>Express</motion.div>
+        <motion.div className={style.skill}>React</motion.div>
+        <motion.div className={style.skill}>Nextjs</motion.div>
+        <motion.div className={style.skill}>Mongodb</motion.div>
+      </motion.div>
+    </motion.div>
   );
 };
 
