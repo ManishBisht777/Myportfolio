21cb5dddeeef235a8a5af11754e85130ed73ebc0
manish <Manishbisht9711@gmail.com>
2022-06-09 21:50:20 +0530

    Apis for Add,update,delete Projects

--- backend/my-portfolio/src/api/project/controllers/project.js
+++ backend/my-portfolio/src/api/project/controllers/project.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- *  product controller
+ *  project controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::product.product');
+module.exports = createCoreController('api::project.project');
--- backend/my-portfolio/src/api/project/services/project.js
+++ backend/my-portfolio/src/api/project/services/project.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * product service.
+ * project service.
  */
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::product.product');
+module.exports = createCoreService('api::project.project');
