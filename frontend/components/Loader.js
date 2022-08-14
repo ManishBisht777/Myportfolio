4344ca71a0f3bb4378adc631b4eef0dd44eccdfc
manish <Manishbisht9711@gmail.com>
2022-08-14 12:00:45 +0530

    more css

---
+++ frontend/components/Loader.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <>
+      <div className="cube"></div>
+      <div className="cube"></div>
+      <div className="cube"></div>
+      <div className="cube"></div>
+      <div className="cube"></div>
+      <div className="cube"></div>
+    </>
+  );
+};
+
+export default Loader;
--- frontend/pages/Home.js
+++ frontend/pages/Home.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AllProjects from "../components/AllProjects";
 import Landingpage from "../components/Landingpage";
 import Skills from "../components/Skills";
 import TimeLine from "../components/TimeLine";
 import TopProjects from "../components/TopProjects";
 import Footer from "../components/Footer";
+import Loader from "../components/Loader";
 const Home = () => {
+  const [loading, setloading] = useState(true);
+
+  useEffect(() => {
+    setloading(false);
+  }, []);
+
   return (
     <>
-      <Landingpage />
-      <TopProjects />
-      <TimeLine />
-      <AllProjects />
-      <Skills />
-      <Footer />
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <Landingpage />
+          <TopProjects />
+          <TimeLine />
+          <AllProjects />
+          <Skills />
+          <Footer />
+        </>
+      )}
     </>
   );
 };
